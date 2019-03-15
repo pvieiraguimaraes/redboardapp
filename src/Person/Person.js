79516a9f2556d5c37908f5e999dc1a87e48127b6
Pedro Henrique <pvieiraguimaraes@gmail.com>
2019-03-15 11:24:15 -0300

    =D

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div className='RedBack'>
+            <p>created it!</p>
+
+            <h4>It really work pretty well =D, i have {props.dollars} dollars in my pocket.</h4>
+        </div>
+    );
+
+};
+
+export default person;
